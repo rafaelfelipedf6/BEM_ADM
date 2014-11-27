ef07b54064ca06d38941099e8308aa9eaf605d55
Rafael Felipe Mendonça Araújo <rafaelfelipedf6@gmail.com>
2014-11-27 14:10:53 -0200

    Traduzindo views do Devise que é o plugin de altenticação de Contas

--- BEM_ADM/db/migrate/20141126204859_devise_create_users.rb
+++ BEM_ADM/db/migrate/20141126204859_devise_create_users.rb
@@ -2,6 +2,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
   def change
     create_table(:users) do |t|
       ## Database authenticatable
+      t.string :nome,               null:false, default: ""
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
@@ -33,7 +34,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
 
       t.timestamps
     end
-
+    add_index :users, :nome,                 unique: true
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
--- BEM_ADM/db/schema.rb
+++ BEM_ADM/db/schema.rb
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20141126204859) do
   end
 
   create_table "users", force: true do |t|
+    t.string   "nome",                   default: "", null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -42,6 +43,7 @@ ActiveRecord::Schema.define(version: 20141126204859) do
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["nome"], name: "index_users_on_nome", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
   create_table "vendedors", force: true do |t|
